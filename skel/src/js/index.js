32c0b3804d11c63783f55c1b960eb4f4e4898c0a
edge <sort@programmer.net>
2015-11-19 03:36:12 +0900

    skip first render on client

--- skel/src/js/index.js
+++ skel/src/js/index.js
@@ -2,6 +2,6 @@ import Cycle from '@cycle/core';
 import { makeDOMDriver } from '@cycle/dom';
 import view from './view';
 
-Cycle.run(({ DOM }) => ({ DOM: view(DOM) }), {
+Cycle.run(({ DOM }) => ({ DOM: view(DOM).skip(1) }), {
 	DOM: makeDOMDriver('#root')
 });
