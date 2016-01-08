268bda1dbbb1a4d10a2e342786ebfbda2e7949de
edge <sort@programmer.net>
2016-01-09 00:49:19 +0900

    install pages

--- gulpfile.babel.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp';
-import babel from 'gulp-babel';
-
-gulp.task('default', () =>
-	gulp.src('./src/**/*')
-		.pipe(babel())
-		.pipe(gulp.dest('./lib'))
-);
--- skel/src/js/index.js
+++
@@ -1,9 +0,0 @@
-import { run } from '@cycle/core';
-import { makeDOMDriver } from '@cycle/dom';
-import mvi from './mvi';
-
-run(({ DOM }) => ({
-	DOM: mvi(DOM).skip(1)
-}), {
-	DOM: makeDOMDriver('#root')
-});
